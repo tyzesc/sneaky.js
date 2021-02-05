c57ed2ad5dd558ab7bca4ae2c44f524bb57d7794
tyzesc <tyzescgm@gmail.com>
2021-02-05 21:49:12 +0800

    update for better usage of every proxy server

--- sneaky.js
+++ sneaky.js
@@ -5,6 +5,16 @@ const HttpsProxyAgent = require('https-proxy-agent');
 const { promisify } = require('util');
 const pause = promisify((a, f) => setTimeout(f, a))
 
+const colors = require("colors/safe")
+
+const FOR_LOOP_DURATION = 1
+
+const MESSAGE = {
+    NORMAL_HEAD: "[*]",
+    NO_ENOUGH_PROXY: colors.bold("目前無空閒伺服器"),
+    NOW_TASK: colors.cyan("目前任務"),
+}
+
 class Proxy {
     constructor(url) {
         this.url = url
@@ -13,6 +23,8 @@ class Proxy {
 
         this.success = 0
         this.errors = 0
+
+        this.retry = 0
     }
 
     getHealth() {
@@ -48,7 +60,15 @@ class RetryError extends Error {
 
 class PauseError extends Error {
     constructor(duration) {
-        super("should sleep " + duration + " seconds")
+        super("should sleep " + duration / 1000 + " seconds")
+        this.duration = duration
+    }
+}
+
+class PRError extends Error {
+    constructor(url, duration) {
+        super("should sleep " + duration / 1000 + " seconds then open ", url)
+        this.url = url
         this.duration = duration
     }
 }
@@ -56,6 +76,7 @@ class PauseError extends Error {
 class Sneaky {
     constructor(proxies) {
         this.proxies = proxies.map(url => new Proxy(url))
+        this.rest_proxies = []
     }
 
     async test() {
@@ -69,61 +90,62 @@ class Sneaky {
     }
 
     async run(urls, options = {}, cb) {
-        let maxLength = 1
-        let runningProxies = []
-        let promises = []
-        let results = []
-
+        let ok = 0
+        let st = Date.now()
         let id = 0
+        let results = []
         for (let url of urls) {
-            console.log(`[*] (${++id}/${urls.length}) ${url}`)
-            let proxy = this.proxies.shift()
-            let res = proxy.get(url, options)
-            runningProxies.push(proxy)
-            promises.push(res)
+            let per = (Date.now() - st) / ok / 1000
+            let est = per * (urls.length - ok)
+            console.log(
+                MESSAGE.NORMAL_HEAD,
+                MESSAGE.NOW_TASK,
+                colors.bold(`(${++id}/${urls.length})`),
+                "PER:", per,
+                "EST:", est,
+                `${url.substr(url.length - 10)}`
+            )
 
-            if (promises.length >= maxLength || id == urls.length) {
-                let arr = await Promise.all(promises)
-                console.log("this time has", promises.length, "entity.")
-
-                // 把 proxy 放回去
-                this.proxies = [...this.proxies, ...runningProxies]
-                runningProxies = []
-
-                for (let a of arr) {
-                    try {
-                        let b = await cb(a)
-                        results = [...results, b]
-                    } catch (e) {
-                        if (e instanceof RetryError) {
-                            console.log(e.message)
-                            urls.push(e.url)
-                        } else if (e instanceof PauseError) {
-                            console.log(e.message)
-                            await pause(e.duration)
-                        } else {
-                            await appendFileSync("sneaky.error", e + '\n')
-                            console.error(e)
-                        }
-                    }
-                }
-
-                promises = []
-
-                // 都成功的話拉高上限
-                if (arr.every(e => !(e instanceof Error))) {
-                    maxLength++
-                    if (maxLength > this.proxies.length)
-                        maxLength = this.proxies.length
-                } else {
-                    console.log("there is error, back to 1")
-                    maxLength = 1
-                }
-
-                console.log("===============================")
+            while (this.proxies.length <= 0) {
+                console.log('[*]', MESSAGE.NO_ENOUGH_PROXY)
+                await pause(5 * 1000)
             }
-        }
 
+            let proxy = this.proxies.shift()
+
+            proxy.get(url, options)
+                .then(cb)
+                .then(result => {
+                    results.push(result)
+                    console.log(MESSAGE.NORMAL_HEAD, colors.gray(`伺服器${proxy}完成任務 ${result} 三秒後回到崗位`))
+                    proxy.retry = 0
+                    ok++
+                    pause(3 * 1000)
+                        .then(() => {
+                            this.proxies.push(proxy)
+                        })
+                })
+                .catch(err => {
+                    if (err instanceof PauseError) {
+                        console.log(MESSAGE.NORMAL_HEAD, colors.grey(`伺服器${proxy}出現問題`), colors.blue(`進入恢復時間 ${proxy.retry * 5}sec`))
+                        pause((proxy.retry++) * 5 * 1000)
+                            .then(() => {
+                                this.proxies.push(proxy)
+                            })
+                    } else if (err instanceof PRError) {
+                        console.log(MESSAGE.NORMAL_HEAD, colors.grey(`伺服器${proxy}出現問題`), colors.blue(`進入恢復時間 ${proxy.retry * 5}sec`))
+                        pause((proxy.retry++) * 5 * 1000)
+                            .then(() => {
+                                this.proxies.push(proxy)
+                            })
+                        urls.push(err.url)
+                    } else {
+                        console.log("不處理", err)
+                    }
+                })
+
+            await pause(FOR_LOOP_DURATION * 1000)
+        }
         return results
     }
 }
@@ -131,3 +153,4 @@ class Sneaky {
 module.exports = Sneaky
 module.exports.RetryError = RetryError
 module.exports.PauseError = PauseError
+module.exports.PRError = PRError
